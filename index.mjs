d56eae4c68bb4465c5d03324e632360d2439c013
auto-aithertechnology <154814037+auto-aithertechnology@users.noreply.github.com>
2024-01-18 21:26:02 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! permissions Heyyyyyyy'),
+    body: JSON.stringify('Hello from Lambda! permissions'),
   };
   return response;
 };
